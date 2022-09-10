f1da671d14490e33c25ab495f5237feb2777e379
catalinfl <catalinfl02@gmail.com>
2022-09-10 14:13:57 +0300

    alert off

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -17,7 +17,7 @@ const List = () => {
   const [options, setOptions] = useState(location.state.options);
   const [min, setMin] = useState(undefined);
   const [max, setMax] = useState(undefined);
-  const { data, loading, empty, error, reFetch } = useFetch(`/hotels?city=${destination.toLowerCase()}&min=${min || 0}&max=${max || 999}`)
+  const { data, loading, empty, error, reFetch } = useFetch(`/hotels?city=${destination}&min=${min || 0}&max=${max || 999}`)
 
   const handleClick = () => {
     reFetch();
